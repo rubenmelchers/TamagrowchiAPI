4d1219188fa9428985b36c134b824f94adcd479a
Ruben Melchers <rubenmelchers1994@gmail.com>
2017-06-25 19:27:33 +0200

    Init API

---
+++ settings.php
@@ -0,0 +1,15 @@
+<?php
+
+// $url_base = "http://$_SERVER[HTTP_HOST]/ondiep/api/";
+$url_base = "https://stud.hosted.hr.nl/0889496/tamagrowchi/api";
+
+// DATABASE VARIABLES - LOCALHOST
+$db_host = "sql.hosted.hr.nl";
+$db_username = "0889496";
+$db_password = "Skwotor1";
+$db_name = "0889496";
+
+
+// CONNECT WITH DATABASE
+$connection = mysqli_connect($db_host, $db_username, $db_password, $db_name)
+or die(mysqli_error($connection));
