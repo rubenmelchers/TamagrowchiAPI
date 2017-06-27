ba6a78953a26e37dd0da3132d788414c2a648e80
Ruben Melchers <rubenmelchers1994@gmail.com>
2017-06-27 20:51:36 +0200

    Added relevant comments

--- data.php
+++ data.php
@@ -7,8 +7,11 @@ $format = $_SERVER['HTTP_ACCEPT'];
 $table = "tamagrowchi";
 $fields = [ 'soil', 'time', 'waterboolean' ];
 
+/* Header for local environment */
 header('Access-Control-Allow-Origin: http://localhost:8888');
 
+/* check if an id, order, start and limit are sent with the url */
+/* Example: ..data.php?id=12 */
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 }
@@ -22,12 +25,21 @@ if (isset($_GET['limit'])) {
     $limit = $_GET['limit'];
 }
 
+/* Check which method is given (GET, POST, PUT, DELETE or OPTIONS) */
 switch ($method) {
     case 'GET':
+
+        /* Query for getting all data from the table */
         $query = "SELECT * FROM $table";
         $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+
+        /* If a limit parameter is found, process the data accordingly */
         if (!empty($limit)) {
+
+            /* Set the page total results according to the given limit */
             $page_total = ceil($result->num_rows / $limit);
+
+            /* If there is not start, set it to 1 */
             if (empty($start)) {
                 $page_current = 1;
             } else {
@@ -45,10 +57,12 @@ switch ($method) {
                 $page_previous = 1;
             }
 
+            /* Set the start results for the last, previous and next pages */
             $url_last_start = $page_last * $limit - $limit + 1;
             $url_previous_start = $page_previous * $limit - $limit + 1;
             $url_next_start = $page_next * $limit - $limit + 1;
 
+            /* Create the url's for the links */
             $url_first = "?start=1&limit=" . $limit;
             $url_last = "?start=" . $url_last_start . "&limit=" . $limit;
             $url_previous = "?start=" . $url_previous_start . "&limit=" . $limit;
@@ -69,12 +83,17 @@ switch ($method) {
 
         $query = "SELECT * FROM $table ";
 
+        /* If the ID parameter is found, change the query to display the results with the matching ID */
         if (!empty($id)) {
             $query .= " WHERE id = " . $id;
         }
+
+        /* If the order parameter is found, change the query to order the results. values are ASC or DESC */
         if (!empty($order)) {
-            $query .= " ORDER BY " . $order;
+            $query .= " ORDER BY id " . $order;
         }
+
+        /* If the limit parameter is found, adjust the query so that there are results as many as the given limit */
         if (!empty($limit)) {
             $query .= " LIMIT ";
             if (!empty($start)) {
@@ -83,9 +102,11 @@ switch ($method) {
             $query .= $limit;
         }
 
+        /* Get results */
         $result_items = mysqli_query($connection, $query) or die(mysqli_error($connection));
 
-        // ARRAY BUILD
+
+        /* ARRAY BUILD */
         $end_array = [];
         $items = [];
         $items_collection = [];
@@ -101,6 +122,7 @@ switch ($method) {
                 Array('rel' => 'previous', 'page' => $page_previous, 'href' => $url_base . $url_previous),
                 Array('rel' => 'next', 'page' => $page_next, 'href' => $url_base . $url_next)
             )];
+
         while ($item = mysqli_fetch_assoc($result_items)) {
             $links = [array('rel' => 'self', 'href' => ($url_base . '/data.php?id=' . $item['id'])), array('rel' => 'collection', 'href' => $url_base)];
             $item['links'] = $links;
@@ -115,21 +137,40 @@ switch ($method) {
         if (empty($id)) {
             $end_array = $items;
         }
-        // ARRAY MADE
+
+
+        /* ARRAY MADE */
+        /* Check if the format is in json */
         if ($format == 'application/json' || $format == 'json' || $format == 'JSON' || $format == "/json" || $format == "/JSON" || $format == " */*" || $format == "application/json, text/javascript, */*; q=0.01" || $format = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8") {
+
+            /* Check if there are results */
             if ($result_items->num_rows == 0) {
+
+                /* Don't display the results and set an 404 error */
                 http_response_code(404);
+
             } else {
                 header('Content-type: application/json');
+
+                /* Display the results on the page */
                 echo json_encode($end_array);
             }
+
+            /* Check if format is in XML */
         } else if ($format == "application/xml" || $format == "text/xml" || $format == "xml" || $format == "XML" || $format == "/xml" || $format == "/XML") {
+
+            /* Check if there are results */
             if ($result_items->num_rows == 0) {
+
+                /* Don't display the results and set an 404 error */
                 http_response_code(404);
+
             } else {
+
                 header('Content-type: application/xml');
-                function array_to_xml($data, &$xml_data)
-                {
+
+                /* Function for creating xml from the results */
+                function array_to_xml($data, &$xml_data) {
                     foreach ($data as $key => $value) {
                         if (is_array($value)) {
                             if (is_numeric($key)) {
@@ -149,6 +190,8 @@ switch ($method) {
                     $end_array_xml = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><items></items>');
                 }
                 array_to_xml($end_array, $end_array_xml);
+
+                /* Display results */
                 echo $end_array_xml->saveXML();
             }
 
@@ -159,15 +202,21 @@ switch ($method) {
         }
         break;
 
+    /* If a POST request is made */
     case 'POST':
+
         if (isset($id)) {
+
+            /* If an ID parameter is given. Set an 405 error */
             http_response_code(405);
+
         } else {
             $data = [];
             $keys = [];
             $values = [];
             $data_check = false;
 
+            /* If the POST is not empty, set it to the variable $data. */
             if (!empty($_POST)) {
                 $data = $_POST;
             } else if (!empty(file_get_contents("php://input"))) {
@@ -175,6 +224,7 @@ switch ($method) {
                 $data = (array)json_decode($jsonData);
             }
 
+            /* Create variables from the given POST data */
             if (!empty($data)) {
                 foreach ($fields as $field) {
                     if (isset($data[$field])) {
@@ -184,12 +234,14 @@ switch ($method) {
                 }
             }
 
+            /* If there are values, continue, else, set a 411 error */
             if (!empty($values)) {
                 $data_check = true;
             } else {
                 http_response_code(411);
             }
 
+            /* The actual POST query and execution */
             if ($data_check == true) {
                 $query = "INSERT INTO `$table` (`" . implode("`,`", $keys) . "`)
                   VALUES ('" . implode("','", $values) . "')";
@@ -206,12 +258,16 @@ switch ($method) {
         }
         break;
 
+    /* If a PUT request is made */
     case 'PUT':
+
+        /* Check if there is an ID parameter given */
         if (isset($id)) {
             $data = [];
             $values = [];
             $data_check = false;
 
+            /* If the PUT is not empty, set it to the variable $data. */
             if (!empty($_POST)) {
                 $data = $_POST;
             } else if (!empty(file_get_contents("php://input"))) {
@@ -219,6 +275,7 @@ switch ($method) {
                 $data = (array)json_decode($jsonData);
             }
 
+            /* Create variables from the given PUT data */
             if (!empty($data)) {
                 foreach ($fields as $field) {
                     if (isset($data[$field])) {
@@ -227,12 +284,14 @@ switch ($method) {
                 }
             }
 
+            /* If there are values, continue, else, set a 411 error */
             if (!empty($values)) {
                 $data_check = true;
             } else {
                 http_response_code(411);
             }
 
+            /* The actual POST query and execution */
             if ($data_check == true) {
                 $query = "UPDATE `$table` SET " . implode(",", $values) . " WHERE `id` = " .$id;
 
@@ -246,22 +305,30 @@ switch ($method) {
             }
 
         } else {
+
+            /* If there is no ID, set error to 405 */
             http_response_code(405);
         }
         break;
 
+    /* If a DELETE request is made */
     case 'DELETE':
+
+        /* If there is an ID parameter set, execute the query */
         if (isset($id)) {
             $query = "DELETE FROM $table WHERE id =$id";
             $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
             http_response_code(204);
 
         } else {
+
+            /* If there is no ID parameter set, don't execute the query but set a 405 error */
             http_response_code(405);
         }
 
         break;
 
+    /* If an OPTIONS request is made */
     case 'OPTIONS':
         if (isset($id)) {
             header('Allow: GET,PUT,DELETE,OPTIONS');
